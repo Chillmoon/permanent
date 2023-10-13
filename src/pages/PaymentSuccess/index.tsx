2565a29a32a428650d02af7d695bba5551404062
TKolomiiets <t.kolomiiets@sharksw.com>
2023-10-13 15:16:26 +0300

    working on payment

--- src/pages/PaymentSuccess/index.tsx
+++ src/pages/PaymentSuccess/index.tsx
@@ -34,57 +34,40 @@ const PaymentSuccessPage = () => {
   };
 
   useEffect(() => {
-    // Получите параметры из URL
     const searchParams = new URLSearchParams(window.location.search);
     const paramsObject = Object.fromEntries(searchParams.entries());
     console.log(searchParams, "searchParams");
     console.log(paramsObject, "paramsObject");
-    // Извлеките необходимые данные
-    const order_id = paramsObject.order_id;
-    const product_id = paramsObject.product_id;
-    const signature = paramsObject.signature;
-
-    // Выведите данные в консоль
-    console.log("order_id:", order_id);
-    console.log("product_id:", product_id);
-    console.log("signature:", signature);
-  }, []);
-
-  // useEffect(() => {
-  //   const searchParams = new URLSearchParams(window.location.search);
-  //   const paramsObject = Object.fromEntries(searchParams.entries());
-  //   console.log(searchParams, "searchParams");
-  //   console.log(paramsObject, "paramsObject");
-  //   const paymentRate = paramsObject.product_id
-  //     ? paramsObject.product_id.slice(-5)
-  //     : "";
-  //   const payedCourse = paramsObject.product_id
-  //     ? paramsObject.product_id.slice(0, -5)
-  //     : "";
-  //   console.log(payedCourse, "payedCourse");
-  //   console.log(paymentRate, "paymentRate");
-  //   if (paramsObject.order_status === "approved") {
-  //     setIsPaymentSuccessful(true);
-  //     if (payedCourse === "eyeliner") {
-  //       const paymentData = {
-  //         rate: paymentRate,
-  //         orderId: paramsObject.order_id,
-  //         signature: paramsObject.signature,
-  //       };
-  //       dispatch(
-  //         setIsPayed({
-  //           eyeliner: paymentData,
-  //         })
-  //       );
-  //       if (userID) {
-  //         savePaymentData(userID, paymentData);
-  //       } else {
-  //         console.log("userID is undefined");
-  //       }
-  //     }
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [userID]);
+    const paymentRate = paramsObject.product_id
+      ? paramsObject.product_id.slice(-5)
+      : "";
+    const payedCourse = paramsObject.product_id
+      ? paramsObject.product_id.slice(0, -5)
+      : "";
+    console.log(payedCourse, "payedCourse");
+    console.log(paymentRate, "paymentRate");
+    if (paramsObject.order_status === "approved") {
+      setIsPaymentSuccessful(true);
+      if (payedCourse === "eyeliner") {
+        const paymentData = {
+          rate: paymentRate,
+          orderId: paramsObject.order_id,
+          signature: paramsObject.signature,
+        };
+        dispatch(
+          setIsPayed({
+            eyeliner: paymentData,
+          })
+        );
+        if (userID) {
+          savePaymentData(userID, paymentData);
+        } else {
+          console.log("userID is undefined");
+        }
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userID]);
 
   const displayName = auth?.currentUser?.displayName;
 
