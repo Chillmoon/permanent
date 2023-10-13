f53154aff28fc127a27d7c58e7739eb85b729fa4
TKolomiiets <t.kolomiiets@sharksw.com>
2023-10-13 14:43:05 +0300

    working on payment

--- src/pages/PaymentSuccess/index.tsx
+++ src/pages/PaymentSuccess/index.tsx
@@ -34,40 +34,62 @@ const PaymentSuccessPage = () => {
   };
 
   useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const paramsObject = Object.fromEntries(searchParams.entries());
-    console.log(searchParams, "searchParams");
-    console.log(paramsObject, "paramsObject");
-    const paymentRate = paramsObject.product_id
-      ? paramsObject.product_id.slice(-5)
-      : "";
-    const payedCourse = paramsObject.product_id
-      ? paramsObject.product_id.slice(0, -5)
-      : "";
-    console.log(payedCourse, "payedCourse");
-    console.log(paymentRate, "paymentRate");
-    if (paramsObject.order_status === "approved") {
-      setIsPaymentSuccessful(true);
-      if (payedCourse === "eyeliner") {
-        const paymentData = {
-          rate: paymentRate,
-          orderId: paramsObject.order_id,
-          signature: paramsObject.signature,
-        };
-        dispatch(
-          setIsPayed({
-            eyeliner: paymentData,
-          })
-        );
-        if (userID) {
-          savePaymentData(userID, paymentData);
-        } else {
-          console.log("userID is undefined");
-        }
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [userID]);
+    // Выполните POST-запрос к URL, на который Fondy отправляет данные
+    fetch("/paymentSuccess", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams(window.location.search).toString(),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Полученные данные из POST-запроса
+        console.log("Данные из POST-запроса:", data);
+
+        // В data будут содержаться order_id, product_id, signature
+        // Можно обратиться к ним так: data.order_id, data.product_id, data.signature
+      })
+      .catch((error) => {
+        console.error("Ошибка при выполнении POST-запроса:", error);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   const searchParams = new URLSearchParams(window.location.search);
+  //   const paramsObject = Object.fromEntries(searchParams.entries());
+  //   console.log(searchParams, "searchParams");
+  //   console.log(paramsObject, "paramsObject");
+  //   const paymentRate = paramsObject.product_id
+  //     ? paramsObject.product_id.slice(-5)
+  //     : "";
+  //   const payedCourse = paramsObject.product_id
+  //     ? paramsObject.product_id.slice(0, -5)
+  //     : "";
+  //   console.log(payedCourse, "payedCourse");
+  //   console.log(paymentRate, "paymentRate");
+  //   if (paramsObject.order_status === "approved") {
+  //     setIsPaymentSuccessful(true);
+  //     if (payedCourse === "eyeliner") {
+  //       const paymentData = {
+  //         rate: paymentRate,
+  //         orderId: paramsObject.order_id,
+  //         signature: paramsObject.signature,
+  //       };
+  //       dispatch(
+  //         setIsPayed({
+  //           eyeliner: paymentData,
+  //         })
+  //       );
+  //       if (userID) {
+  //         savePaymentData(userID, paymentData);
+  //       } else {
+  //         console.log("userID is undefined");
+  //       }
+  //     }
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [userID]);
 
   const displayName = auth?.currentUser?.displayName;
 
