39aaafa5e31dab939bac0ea6b75173e960a9624a
TKolomiiets <t.kolomiiets@sharksw.com>
2023-12-06 12:23:19 +0200

    fixed login redirect on refresh

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,10 +1,13 @@
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
-
 import { RootState } from "../../app/store";
 
 const ProtectedRoutes = () => {
   const user = useSelector((state: RootState) => state.user.user);
+
+  if (user === null) {
+    return null;
+  }
 
   return user ? <Outlet /> : <Navigate to="/login" />;
 };
